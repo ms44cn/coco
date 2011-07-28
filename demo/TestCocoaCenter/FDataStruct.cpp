a539712a4ba5801d94d883e74893f3416788badc
ms44cn <ms44cnleon@gmail.com>
2011-07-27 19:46:29 -0700

    submit test

---
+++ demo/TestCocoaCenter/FDataStruct.cpp
@@ -0,0 +1,15 @@
+/*
+ *  FDataStruct.cpp
+ *  TestCocoaCenter
+ *
+ *  Created by fuqifacai on 7/27/11.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+
+#include "FDataStruct.h"
+
+FDataStruct::FDataStruct(void)
+{
+	
+}
---
+++ demo/TestCocoaCenter/FDataStruct.h
@@ -0,0 +1,15 @@
+/*
+ *  FDataStruct.h
+ *  TestCocoaCenter
+ *
+ *  Created by fuqifacai on 7/27/11.
+ *  Copyright 2011 __MyCompanyName__. All rights reserved.
+ *
+ */
+class FDataStruct
+{
+	public :
+	FDataStruct();
+	
+	private:
+};
